2aef754204dc6c884c30f6f753054097e657eda2
MunKiJeon <jmk070994@gmail.com>
2022-08-14 13:50:17 +0900

    브랜치 test 겸 매니저 치팅

--- moveingAlone_proj/src/main/java/com/map/manager/Matching.java
+++ moveingAlone_proj/src/main/java/com/map/manager/Matching.java
@@ -3,9 +3,9 @@ package com.map.manager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class StaffManagement implements ManagerService {
+public class Matching implements ManagerService {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		request.setAttribute("mainUrl", "manager/staffmanagement.jsp");
+		request.setAttribute("mainUrl", "manager/matching.jsp");
 	}
 }
